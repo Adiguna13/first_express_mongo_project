8b73e7856127b2b2e37d4d37e7a431cc9d5a5e56
Adiguna <adigunarizqi@gmail.com>
2023-07-17 05:41:03 +0700

    first setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/shop_db")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log(`Shop App listening on http://127.0.0.1:3000`);
+});
