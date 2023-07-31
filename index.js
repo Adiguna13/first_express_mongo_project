30d06af9795b6f418469834291c523085b9a5ae7
Adiguna <adigunarizqi@gmail.com>
2023-07-31 09:01:58 +0700

    Create data product ke model garment

--- index.js
+++ index.js
@@ -67,6 +67,27 @@ app.get(
   })
 );
 
+app.get("/garments/:garment_id/product/create", (req, res) => {
+  const { garment_id } = req.params;
+  res.render("products/create", { garment_id });
+});
+
+app.post(
+  "/garments/:garment_id/product",
+  wrapAsync(async (req, res) => {
+    const { garment_id } = req.params;
+    const garment = await Garment.findById(garment_id);
+    const product = new Product(req.body);
+    console.log(product);
+    console.log(garment_id);
+    garment.products.push(product);
+    await garment.save();
+    await product.save();
+    console.log(garment);
+    res.redirect(`/garments/${garment_id}`);
+  })
+);
+
 app.get("/products", async (req, res) => {
   const { category } = req.query;
   if (category) {
